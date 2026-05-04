b9ccefd8a3b560da59db8f6eee874a03369d7463
AgapovaMariya <lps.mashka@gmail.com>
2026-05-04 17:58:05 +0700

    Ручной DI

--- app/src/main/java/com/example/cafe/CafeApplication.kt
+++ app/src/main/java/com/example/cafe/CafeApplication.kt
@@ -1,9 +1,32 @@
+// CafeApplication.kt
 package com.example.cafe
 
 import android.app.Application
+import com.example.cafe.data.repository.DialogRepository
+import com.example.cafe.domain.usecases.LoadDialogUseCase
+import com.example.cafe.domain.usecases.SaveDialogProgressUseCase
+import com.example.cafe.presentation.screens.dialog.DialogViewModelFactory
 
 class CafeApplication : Application() {
+
+    lateinit var loadDialogUseCase: LoadDialogUseCase
+        private set
+    lateinit var saveDialogProgressUseCase: SaveDialogProgressUseCase
+        private set
+    lateinit var dialogViewModelFactory: DialogViewModelFactory   // <- это поле должно быть
+        private set
+
     override fun onCreate() {
         super.onCreate()
+
+        // Репозиторий
+        val dialogRepository = DialogRepository(applicationContext)
+
+        // UseCase
+        loadDialogUseCase = LoadDialogUseCase(dialogRepository)
+        saveDialogProgressUseCase = SaveDialogProgressUseCase(dialogRepository)
+
+        // Фабрика ViewModel
+        dialogViewModelFactory = DialogViewModelFactory(loadDialogUseCase, saveDialogProgressUseCase)
     }
 }
